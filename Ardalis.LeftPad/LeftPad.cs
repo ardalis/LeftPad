6180cdacc5f820b8974254d30feab8c7e10512b4
Steve Smith <steve@kentsmiths.com>
2023-08-11 16:35:54 -0400

    initial files

---
+++ Ardalis.LeftPad/LeftPad.cs
@@ -0,0 +1,10 @@
+﻿namespace Ardalis.LeftPad
+{
+    public static class ArdalisExtensions
+    {
+        public static string LeftPad(this string input, int totalWidth, char padCharacter = ' ')
+        {
+            return input.PadLeft(totalWidth, padCharacter);
+        }
+    }
+}
